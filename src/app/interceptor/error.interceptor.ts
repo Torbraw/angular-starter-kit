c1621c94a229e6c310169e0b257115de9468ae18
Maxime <gendron.maxime1@gmail.com>
2020-08-11 22:33:35 -0400

    2spaces indent

--- src/app/interceptor/error.interceptor.ts
+++ src/app/interceptor/error.interceptor.ts
@@ -6,15 +6,15 @@ import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authService.logout();
-            }
-            return throwError(err.error);
-        }));
-    }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      if (err.status === 401) {
+        // auto logout if 401 response returned from api
+          this.authService.logout();
+      }
+      return throwError(err.error);
+    }));
+  }
 }
--- src/app/interceptor/jwt.interceptor.ts
+++ src/app/interceptor/jwt.interceptor.ts
@@ -6,20 +6,20 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add auth header with jwt if user is logged in and request is to api url
-        const currentUser = this.authService.currentUserValue;
-        const isLoggedIn = currentUser && currentUser.token;
-        const isApiUrl = request.url.startsWith(environment.webApiEndPoint);
-        if (isLoggedIn && isApiUrl) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: currentUser.token
-                }
-            });
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // add auth header with jwt if user is logged in and request is to api url
+    const currentUser = this.authService.currentUserValue;
+    const isLoggedIn = currentUser && currentUser.token;
+    const isApiUrl = request.url.startsWith(environment.webApiEndPoint);
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: currentUser.token
         }
-        return next.handle(request);
+      });
     }
+    return next.handle(request);
+  }
 }
